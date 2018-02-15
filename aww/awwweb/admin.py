ac043097fda8cf8c0884d7ac5558296e8c42ec8a
YJDave <yashashvidave@gmail.com>
2018-02-15 19:01:35 +0530

    admin: Register model to admin.

--- aww/awwweb/admin.py
+++ aww/awwweb/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+	AppUser,
+)
 
 # Register your models here.
+
+class AppUserAdmin(admin.ModelAdmin):
+	list_display = ["user", "joined_date", "legal_name"]
+	class Meta:
+		model = AppUser
+
+admin.site.register(AppUser, AppUserAdmin)
