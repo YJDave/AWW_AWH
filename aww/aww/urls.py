e3da9a14b39739879f6bf4ef54c7238cda9bdd91
YJDave <yashashvidave@gmail.com>
2018-02-15 18:17:40 +0530

    views: Add home page view.

--- aww/aww/urls.py
+++ aww/aww/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from awwweb.views import (
+	home,
+)
 
 urlpatterns = [
+	url(r'^$', home, name="home"),
     url(r'^admin/', admin.site.urls),
 ]
