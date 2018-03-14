d31cb99453eec52414c57a520005f387b847e6fb
YJDave <yashashvidave@gmail.com>
2018-03-14 20:37:18 +0530

    Add models.

--- aww_vishwa/aww/models.py
+++ aww_vishwa/aww/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+class Admin(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Center(models.Model):
+	name = models.CharField(max_length=250)
+	address = models.CharField(max_length=1000, default="", blank=True)
+	admins = models.ForeignKey(Admin, on_delete=models.CASCADE)
+
+class Subscribers(models.Model):
+	phone_no = PhoneNumberField()
+
+class Vacancy(models.Model):
+	center = models.OneToOneField(Center)
+	created_date = models.DateTimeField(auto_now_add=True)
+
+class Application(models.Model):
+	center = models.OneToOneField(Center)
+	POSITION_CHOICES = (
+		('w', "worker"),
+		('h', "heler"),
+	)
+	for_position = models.CharField(max_length=1, choices=POSITION_CHOICES, default='w')
+	name = models.CharField(max_length=250)
+	phone_no = PhoneNumberField()
+	GRADUATION_CHOICES = (
+		('D', '7 pass'),
+		('C', '10 pass'),
+		('B', '12 pass'),
+		('A', 'graduate'),
+	)
+	graduation = models.CharField(max_length=1, choices=GRADUATION_CHOICES, default='D')
+	is_married = models.BooleanField(default=True)
+	village = models.CharField(max_length=300)
+	area = models.CharField(max_length=300)
+	ward = models.CharField(max_length=300)
+	pincode = models.PositiveIntegerField(default=0)
+	has_birth_certi = models.BooleanField(default=False)
+	has_marriage_certi = models.BooleanField(default=False)
+	has_rashon_certi = models.BooleanField(default=False)
+	has_adhaar_certi = models.BooleanField(default=False)
+	has_waters_certi = models.BooleanField(default=False)
--- aww_vishwa/aww_vishwa/settings.py
+++ aww_vishwa/aww_vishwa/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'aww',
     'adminsite',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
